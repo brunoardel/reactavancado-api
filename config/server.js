4e4789e32b689867dabd10a4008d146755acd312
Bruno Ardel <djbrunohardel@hotmail.com>
2020-10-08 00:33:43 -0300

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8e4d7dd8b339d102392aeaa932827d01'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0f467dd7-bd46-431e-acbd-032325886955'
+};
