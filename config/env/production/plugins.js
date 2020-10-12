8521002ade95b5bf3b54067e2c62af125fdadb87
Bruno Ardel <djbrunohardel@hotmail.com>
2020-10-12 01:46:34 -0300

    add aws s3

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,12 +1,13 @@
 module.exports = ({ env }) => ({
-  // ...
   upload: {
-    provider: "cloudinary",
+    provider: "aws-s3",
     providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: "aws-region",
+      params: {
+        Bucket: "my-bucket",
+      },
     },
   },
-  // ...
 });
